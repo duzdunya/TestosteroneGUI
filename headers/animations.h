16e175758e99f7f3d95bb6d09e41b82a870cf768
cineali <vodbwi@gmail.com>
2026-01-25 20:04:40 +0300

    Move animation to here

--- headers/animations.h
+++ headers/animations.h
@@ -1,8 +1,55 @@
 #ifndef TESTO_ANIMATIONS_H
 #define TESTO_ANIMATIONS_H
-#include "elements.h"
 
-void toggleAnimation(TESTO_Animation *);
+#include <stdlib.h>
+#include <stdint.h>
+#include <SDL2/SDL.h>
+
+enum TESTO_ANIMATION_TYPES {
+  TESTO_BASE_ANIMATION,
+  TESTO_POS_ANIMATION,
+  TESTO_SIZE_ANIMATION,
+  TESTO_OPACITY_ANIMATION,
+};
+
+enum TESTO_ALPHA_TYPES {
+TESTO_ALPHA_IM,
+TESTO_ALPHA_DM
+}
+
+typedef struct {
+  int alpha_a;
+  int alpha_b;
+} TESTO_IM;
+
+typedef struct {
+  double alpha_a;
+  double alpha_b;
+} TESTO_DM;
+
+typedef union {
+  TESTO_IM im;
+  TESTO_DM dm;
+} TESTO_M;
+
+typedef struct {
+  enum TESTO_ANIMATION_TYPES type;
+  int duration;
+  int start_delay;
+  _Bool reverse;
+  double (*easing_function)(double);
+  TESTO_ALPHA_TYPES base_type;
+  TESTO_M base;
+
+  //internal
+  _Bool active;
+  _Bool ended;
+  _Bool internal_reverse;
+  double progress;
+  Uint32 startTick;
+} TESTO_Animation;
+
+void toggleAnimation(TESTO_Animation*);
 void stopAnimation(TESTO_Animation*);
 void continueAnimation(TESTO_Animation*);
 
--- src/animations.c
+++ src/animations.c
@@ -1,21 +1,99 @@
 #include "animations.h"
 
-void toggleAnimation(TESTO_Animation *animation) {
-	if (animation->ended){
-		animation->reverse = true;
-		animation->ended = false;
-		}
-	else animation->reverse = false;
-    animation->active = 1;
-	animation->progress = 0.0;
-    animation->startTick = SDL_GetTicks64();
+void toggleAnimation(TESTO_Animation* animation) {
+  if (animation->ended) {
+    animation->ended = false;
+  }
+  animation->reverse = !animation->reverse;
+  animation->active = 1;
+  animation->progress = 0.0;
+  animation->startTick = SDL_GetTicks64();
+}
+
+void stopAnimation(TESTO_Animation* animation) { animation->active = 0; }
+
+void continueAnimation(TESTO_Animation* animation) { animation->active = 1; }
+
+
+// ANIMATION CREATION SECTION
+TESTO_Animaton _createBaseAnimation(int duration, int start_delay,
+                                    _Bool reverse,
+                                    double (*easing_func)(double)) {
+  if (easing_func == NULL) easing_func = easeInOutSine;
+  TESTO_Animation animationToReturn = {
+      .duration = duration,
+      .start_delay = start_delay,
+      .reverse = reverse,
+      .easing_function = easing_func,
+
+      .ended = 0,
+      .active = 0,
+      .internal_reverse = 0,
+      .progress = 0.0,
+  };
+
+  return animationToReturn;
+}
+
+// DOUBLE POSITION ANIMATION
+TESTO_Animation createPositionAnimation(
+    double alpha_x, double alpha_y, int duration, int start_delay,
+    _Bool reverse double (*easing_func)(double)) {
+  TESTO_Animation animationToReturn =
+      _createBaseAnimation(duration, start_delay, reverse, easing_func);
+  // DOUBLE M
+  TESTO_DM DM_to_set = {alpha_x, alpha_y};
+  TESTO_M M_to_set = {.dm = DM_to_set};
+  animationToReturn.type = TESTO_POS_ANIMATION;
+  animationToReturn.base_type = TESTO_ALPHA_DM;
+  animationToReturn.base = M_to_set;
+  return animationToReturn;
+}
+
+// INTEGER POSITION ANIMATION
+TESTO_Animation createPositionAnimationInt(
+    int alpha_x, int alpha_y, int duration, int start_delay,
+    _Bool reverse double (*easing_func)(double)) {
+  TESTO_Animation animationToReturn =
+      _createBaseAnimation(duration, start_delay, reverse, easing_func);
+  // INTEGER M
+  TESTO_IM IM_to_set = {alpha_x, alpha_y};
+  TESTO_M M_to_set = {.im = IM_to_set};
+  animationToReturn.type = TESTO_POS_ANIMATION;
+  animationToReturn.base_type = TESTO_ALPHA_IM;
+  animationToReturn.base = M_to_set;
+  return animationToReturn;
+}
+
+// DOUBLE SIZE ANIMATION
+TESTO_Animation createSizeAnimation(
+    double alpha_w, double alpha_h, int duration, int start_delay,
+    _Bool reverse double (*easing_func)(double)) {
+  TESTO_Animation animationToReturn =
+      _createBaseAnimation(duration, start_delay, reverse, easing_func);
+  // DOUBLE M
+  TESTO_DM DM_to_set = {alpha_w, alpha_h};
+  TESTO_M M_to_set = {.dm = DM_to_set};
+  animationToReturn.type = TESTO_SIZE_ANIMATION;
+  animationToReturn.base_type = TESTO_ALPHA_DM;
+  animationToReturn.base = M_to_set;
+  return animationToReturn;
+}
+
+// INTEGER SIZE ANIMATION
+TESTO_Animation createSizeAnimationInt(
+    int alpha_w, int alpha_h, int duration, int start_delay,
+    _Bool reverse double (*easing_func)(double)) {
+  TESTO_Animation animationToReturn =
+      _createBaseAnimation(duration, start_delay, reverse, easing_func);
+  // INTEGER M
+  TESTO_DM IM_to_set = {alpha_w, alpha_h};
+  TESTO_M M_to_set = {.im = IM_to_set};
+  animationToReturn.type = TESTO_SIZE_ANIMATION;
+  animationToReturn.base_type = TESTO_ALPHA_IM;
+  animationToReturn.base = M_to_set;
+  return animationToReturn;
 }
 
 
-void stopAnimation(TESTO_Animation* animation){
-	animation->active = 0;
-}
-
-void continueAnimation(TESTO_Animation* animation){
-	animation->active = 1;
-}
+// ANIMATION CREATION SECTION END
