702743efb1fb4f25a9d6b24d4d5ba66a5f543218
cineali <vodbwi@gmail.com>
2026-02-02 01:39:38 +0300

    align.h merged to calculations.

--- headers/align.h
+++
@@ -1,7 +0,0 @@
-#ifndef ALIGN_H
-#define ALIGN_H
-
-int ratioInterpolate(double ratio, int fromNumber, int toNumber);
-double doubleInterpolate(double ratio, double fromNumber, double toNumber);
-
-#endif
